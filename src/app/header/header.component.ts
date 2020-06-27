85d2fe3e7fe9b16c4d33445d5eb8a9c3d992ba43
Avni Genç <avnignc@gmail.com>
2020-06-27 18:01:41 +0300

    header fix for authenticated users

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../services/storage.service';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  loading = false;
 
-  constructor() { }
+  constructor(public storageService: StorageService) { }
 
   ngOnInit() {}
 
+  logout() {
+    this.loading = true;
+    timer(500).subscribe((sec) => {
+      this.storageService.logout();
+      this.loading = false;
+      location.reload();
+    });
+  }
 }
