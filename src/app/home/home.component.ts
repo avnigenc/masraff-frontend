5a2ba8e13af5c6b03009e96df2dd94e68b6eac7a
Avni Genç <avnignc@gmail.com>
2020-06-27 18:02:33 +0300

    home fetched owned expenses

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../services/storage.service';
+import { ExpenseService } from '../services/expense.service';
+import { Expense } from '../models/business/expense.model';
+import { User } from '../models/business/user.model';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  myExpenses: Expense[];
+  isAuth = false;
+  user: User;
+
+  constructor(
+    private storageService: StorageService,
+    private expenseService: ExpenseService,
+    private authService: AuthService
+  ) {
+    this.isAuth = this.storageService.isAuthenticated();
+    if (this.isAuth) {
+      this.authService.getCurrentUser().subscribe((response) => {
+        console.log(response);
+        this.user = response;
+        this.getAllExpensesById(this.user.id);
+      }, (error: any) => {
+        console.log(error);
+      });
+    }
+  }
 
   ngOnInit() {
+  }
+
+  getAllExpensesById(id: string) {
+    this.expenseService.getAllExpensesById(id).subscribe((response) => {
+      console.log(response);
+      this.myExpenses = response;
+    }, (error: any) => {
+      console.log(error);
+    });
   }
 
 }
