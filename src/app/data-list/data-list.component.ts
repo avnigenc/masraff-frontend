8bc2237cf1f82a12225020f5bff02e9acef87643
Avni Genç <avnignc@gmail.com>
2020-06-27 10:17:14 +0300

    data list component

---
+++ src/app/data-list/data-list.component.ts
@@ -0,0 +1,102 @@
+import { Component, OnInit } from '@angular/core';
+import { EditButtonComponent } from '../atomic/edit-button/edit-button.component';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateOrUpdateExpenseComponent } from '../molecular/create-or-update-expense/create-or-update-expense.component';
+import { MetaDataService } from '../services/meta-data.service';
+import { Expense } from '../models/business/expense.model';
+import { Currency } from '../models/business/currency.model';
+import { VatRate } from '../models/business/vat-rate.model';
+
+@Component({
+  selector: 'app-data-list',
+  templateUrl: './data-list.component.html',
+  styleUrls: ['./data-list.component.scss']
+})
+export class DataListComponent implements OnInit {
+
+  settings = {
+    columns: {
+      companyName: {
+        title: 'Şirket'
+      },
+      totalAmount: {
+        title: 'Toplam tutar'
+      },
+      vatAmount: {
+        title: 'KDV tutarı'
+      },
+      vatRate: {
+        title: 'KDV oranı',
+        valuePrepareFunction: (vatRate) => {
+          return vatRate.amount;
+        },
+      },
+      currency: {
+        title: 'Para birimi',
+        valuePrepareFunction: (currency) => {
+          return currency.name;
+        },
+      },
+      button: {
+        title: '',
+        type: 'custom',
+        filter: false,
+        renderComponent: EditButtonComponent,
+        onComponentInitFunction: (instance) => {
+          instance.save.subscribe((row) => {
+            this.openDialog(row);
+          });
+        }
+      }
+    },
+    actions: {
+      add: false,
+      edit: false,
+      delete: false,
+      position: 'right'
+    },
+    noDataMessage: 'masraf yok',
+  };
+
+  expenses: Expense[];
+  currencies: Currency[];
+  vatRates: VatRate[];
+
+  constructor(public dialog: MatDialog, private metaDataService: MetaDataService) { }
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    this.metaDataService.getAllExpenses().subscribe((response) => {
+      this.expenses = response;
+    }, (error: any) => {
+      console.log(error);
+    });
+
+    this.metaDataService.getAllVatRates().subscribe((response) => {
+      this.vatRates = response;
+    }, (error: any) => {
+      console.log(error);
+    });
+
+    this.metaDataService.getAllCurrencies().subscribe((response) => {
+      this.currencies = response;
+    }, (error: any) => {
+      console.log(error);
+    });
+  }
+
+  openDialog(row: any): void {
+    const dialogRef = this.dialog.open(CreateOrUpdateExpenseComponent, {
+      width: '400px',
+      data: { expense: row, meta: { currencies: this.currencies, vatRates: this.vatRates } }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+}
